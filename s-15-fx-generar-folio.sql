d186ed4b86c773d1847af25fe93499856b4e411e
Lesliee Sarahi <143286669+LesliSara@users.noreply.github.com>
2024-06-08 06:58:20 -0600

    [FIX] Se corrigieron todos los errores, esta es la final

--- s-15-fx-generar-folio.sql
+++ s-15-fx-generar-folio.sql
@@ -7,16 +7,23 @@ create or replace function generar_folio_pase_abordo (
   p_vuelo_pasajero_id number
 ) return varchar2 is
   v_folio_abordo varchar2(8);
+  v_pase_abordo_id number;
+  v_random_string varchar2(4);
 begin
+  -- Obtener el próximo valor de la secuencia
   select pase_abordo_seq.nextval into v_pase_abordo_id from dual;
-    
-  -- genera un folio único basado en el pase_abordo_id y un sufijo aleatorio
-  v_folio_abordo := to_char(v_pase_abordo_id) || '-' || dbms_random.string('x', 8);
 
-  insert into pase_abordo (pase_abordo_id,folio_abordo, vuelo_pasajero_id) 
-	values (v_pase_abordo_id, v_folio_abordo, p_vuelo_pasajero_id);
+  -- Generar un sufijo aleatorio de 4 caracteres
+  v_random_string := dbms_random.string('x', 4);
+
+  -- Tomar los últimos 4 dígitos del pase_abordo_id y concatenar con la cadena aleatoria
+  v_folio_abordo := v_random_string || to_char(v_pase_abordo_id, 'FM0000');
+
+  -- Insertar el nuevo pase de abordo
+  insert into pase_abordo (pase_abordo_id, folio_abordo, vuelo_pasajero_id) 
+  values (v_pase_abordo_id, v_folio_abordo, p_vuelo_pasajero_id);
 
   return v_folio_abordo;
 end;
 /
-show errors
+show errors;
--- s-16-fx-generar-folio-prueba.sql
+++ s-16-fx-generar-folio-prueba.sql
@@ -4,18 +4,15 @@
 --@Descripción:     Prueba funcion que genera folios de pase de abordar
 
 declare
-  v_vuelo_pasajero_id number;
   v_folio varchar2(8);
 begin
   -- insertar un vuelo_pasajero de prueba
-	insert into vuelo_pasajero (vuelo_pasajero_id, num_asiento, presento, 
-  	atencion_especial, pasajero_id, vuelo_id)
-	values (1000, 13, 1, null, 4, 1);
   insert into vuelo_pasajero (vuelo_pasajero_id, num_asiento, presento, atencion_especial,
     pasajero_id, vuelo_id)
-	values (1010, 2, 1, null, 8, 1);
+	values (1011, 2, 1, null, 8, 1);
+  
   -- llamar a la función para generar el pase de abordo
-  v_folio := generar_folio_pase_abordo(v_vuelo_pasajero_id);
+  v_folio := generar_folio_pase_abordo(1010);
 
   -- mostrar el resultado
   dbms_output.put_line(v_folio);
